b1a5d3d0fcfadc67a06159d141ac133d69beba62
Maggie Maurer <87448603+maurermaggie@users.noreply.github.com>
2022-04-07 17:19:04 -0700

    Update meta_data_cleaning.R

--- meta_data_cleaning.R
+++ meta_data_cleaning.R
@@ -9,119 +9,30 @@ library(stringr)
 
 
 ##############################################################################################################
+################################-------REARRANGE CLOCK METADATA FILE-------###################################
+##############################################################################################################
+#the clock metadata file has three columns: tissue, id (428 unique), and mean_dage_resid_zscored
+#I want to have one row per sample id, which contians all of the mean residual zscores for each tissue (each in a seperate column)
+clock_metadata_file <-"filepath"
+clock_metadata <- read_csv(clock_metadata_file)
+proteomics <- pivot_wider(clock_metadata, names_from = "tissue", values_from = "mean_dage_resid_zscored")
+colnames(proteomics)[1] <- "Barcode"
+
+##############################################################################################################
 ##################################-------ADD CLOCK INFO TO METADATA-------####################################
 ##############################################################################################################
-clock_metadata_file <-"/Users/maurertm/Downloads/ADRC_Enriched_TissueClocks_Means_2022-03-14.csv"
-clock_metadata <- read_csv(clock_metadata_file)
-
-unique_tissues <- clock_metadata$tissue %>% unique()
-
-unique_tissues_list <- c()
-
-for (i in unique_tissues) {
-  df <- filter(clock_metadata, tissue == i)
-  nam <- paste("df", i, sep = "_")
-  assign(nam, df)
-  unique_tissues_list[[nam]] <-df
-}
-unique_tissues_list_edited <- lapply(unique_tissues_list, function(df) select(df, -1))
-
-counter=0
-for (i in unique_tissues_list_edited){
-  counter = counter + 1
-  list_value<-names(unique_tissues_list_edited[counter])
-  zscore_val <- paste(list_value, "mean_dage_resid_zscored", sep="_")
-  colnames(unique_tissues_list_edited[[counter]])=c("Barcode", zscore_val)
-}
-
-combined <- reduce(unique_tissues_list_edited, full_join, by = "Barcode")
-
-colnames(combined)<-gsub("df_","",colnames(combined))
-
-metadata_file <-"/Users/maurertm/Downloads/MetaData.csv"
+metadata_file <-"/Users/maurertm/Desktop/Projects/ADRC/Metadata/Plasma_metadata_FINAL_052021_ADRC_additionalQC_new.csv"
 metadata <- read_csv(metadata_file)
 
-proteomics_ID_list <- combined$Barcode
-metadata_ID_list <- metadata$Barcode
-
-proteomics_in_metadata <- filter(metadata, Barcode %in% proteomics_ID_list)
-proteomics_not_in_metadata <- filter(metadata, !Barcode %in% proteomics_ID_list)
-setdiff(proteomics_ID_list, metadata_ID_list)
-#^this should be zero, which it is 
-
-proteomics_in_metadata_merged_age <- merge(proteomics_in_metadata, combined, by="Barcode")
-proteomics_colnames <- colnames(proteomics_in_metadata_merged_age)
-
-missing_proteomics_colnames <- setdiff(proteomics_colnames, non_proteomics_colnames)
-
-proteomics_not_in_metadata[ , missing_proteomics_colnames] <- NA
-
-proteomics_added_to_metadata_df <- rbind(proteomics_in_metadata_merged_age, proteomics_not_in_metadata)
-
-proteomics_distinct_rows <- distinct(proteomics_added_to_metadata_df) 
-
-##############################################################################################################
-################################-------GET DFs WITH ALL METADATA IDS-------################################
-##############################################################################################################
-metadata_sample <- proteomics_distinct_rows %>% 
-  dplyr::select(SampleId, ADRC_ID, PIDN) %>% 
-  mutate(SampleId_nospace = sub(" .*", "", SampleId)) 
-
-#1. three dataframes for the three different types of IDs
-metadata_sample$index <- rownames(metadata)
-ADRC_ID <- metadata_sample %>% dplyr::select(ADRC_ID, index)
-PIDN_ID <- metadata_sample %>% dplyr::select(PIDN, index)
-SI <- metadata_sample %>% dplyr::select(SampleId, index)
-
-#2. Clean the data frames
-#SI- remove the space after the numbers and put into a seperate column
-SI_cleaned1 <- separate(SI, SampleId, into = c("SampleId", "visit_details"), sep = " (?=[^ ]+$)")
-SI_cleaned2 <- separate(SI_cleaned1, SampleId, into = c("SampleId", "visit_details2"), sep = " (?=[^ ]+$)")
-SI_cleaned2$details_visit <- str_c(SI_cleaned2$visit_details, " ", SI_cleaned2$visit_details2)
-SI_cleaned2$index_val <- str_c(SI_cleaned2$index, "_", "SI")
-#remove the 0 before the numbers
-SI_cleaned2$ID_nozero <- str_remove_all(SI_cleaned2$SampleId, "^0")
-SI_cleaned2 <- SI_cleaned2[order(SI_cleaned2$SampleId), ]
-SI_cleaned_final <- SI_cleaned2 %>% dplyr::select(ID_nozero, index, index_val, details_visit)
-
-colnames(SI_cleaned_final) <- c("ID", "index", "index_val", "details_visit")
-
-
-#ADRC_ID- no cleaning needed, except add key to index and details visit column
-ADRC_ID_cleaned_final <- ADRC_ID
-ADRC_ID_cleaned_final$index_val <- str_c(ADRC_ID_cleaned_final$index, "_", "ADRC")
-ADRC_ID_cleaned_final$details_visit <- NA
-colnames(ADRC_ID_cleaned_final) <- c("ID", "index", "index_val", "details_visit")
-
-#PIDN_ID
-#I need to convert all of the values that can be numbers to numbers
-PIDN_numbers <- filter(PIDN_ID, !is.na(as.numeric(PIDN)))
-PIDN_letters <- filter(PIDN_ID, is.na(as.numeric(PIDN)))
-
-PIDN_numbers[, 1] <- sapply(PIDN_numbers[, 1], as.numeric)
-
-PIDN_cleaned_final <- rbind(PIDN_numbers, PIDN_letters)
-PIDN_cleaned_final$index_val <- str_c(PIDN_cleaned_final$index, "_", "PIDN")
-PIDN_cleaned_final$details_visit <- NA
-colnames(PIDN_cleaned_final) <- c("ID", "index", "index_val", "details_visit")
-
-
-#3. Combine all three dataframes 
-metadata_ids <- rbind(PIDN_cleaned_final, ADRC_ID_cleaned_final, SI_cleaned_final)
-metadata_id_df <- dplyr::select(metadata_ids, ID)
-
-##############################################################################################################
-################################-------GET LIST WITH ALL METADATA IDS-------################################
-##############################################################################################################
-metadata_id_list <- dplyr::select(metadata_ids, ID) %>% as.list()
-metadata_id_unnammed <- unname(metadata_id_list) %>% unlist()
+metadata_proteomics <- left_join(metadata, proteomics)
 
 
 ##############################################################################################################
 ################################-------GET LIST WITH ALL GENOTYPED IDS-------#################################
 ##############################################################################################################
-sample_list <- "/Users/maurertm/Downloads/files.txt"
-sample_list2 <- "/Users/maurertm/Downloads/files_R.tsv"
+sample_list <- "//Users/maurertm/Desktop/Projects/ADRC/file_lists/files.txt"
+sample_list2 <- "/Users/maurertm/Desktop/Projects/ADRC/file_lists/files_R.tsv"
+
 
 samples<- read_tsv(sample_list, col_names=FALSE)
 samples_unique <- samples[!duplicated(samples[ , "X1"]), ] 
@@ -143,10 +54,82 @@ samples_no_underscore$ID_nospace <- str_remove_all(samples_no_underscore$ID, "^0
 genotypes <- dplyr::select(samples_no_underscore, ID_nospace)
 colnames(genotypes) <- "ID"
 
+metadata_proteomics$PIDN_cleaned <- str_remove_all(metadata_proteomics$PIDN, "^0") %>%
+  str_remove_all(" .*$") %>% str_remove_all("_.*$") %>% str_remove_all("[A-Z]")
+
+metadata_proteomics$SampleID_cleaned <- str_remove_all(metadata_proteomics$SampleID, "^0") %>%
+  str_remove_all(" .*$") %>% str_remove_all("_.*$") %>% str_remove_all("[A-Z]")
+
+metadata_proteomics$ADRC_ID_cleaned <- str_remove_all(metadata_proteomics$ADRC_ID, "^0") %>%
+  str_remove_all(" .*$") %>% str_remove_all("_.*$") %>% str_remove_all("[A-Z]")
+
+metadata_ids$match <- as.numeric(metadata_ids$ID %in% genotypes_filtered_list_unnammed)
+
+metadata_proteomics_no_plus <- filter(metadata_proteomics, !str_detect(PIDN_cleaned, "\\+"))
+metadata_proteomics_plus <- filter(metadata_proteomics, str_detect(PIDN_cleaned, "\\+"))
+
+fix_id <- function(row, row_df) {
+  duplicated <- bind_rows(row, row)
+  fixed_pidn <- str_split_fixed(duplicated$PIDN_cleaned[1], "\\+", 2)
+  fixed_sampleid <- str_split_fixed(duplicated$SampleID_cleaned[1], "\\+", 2)
+  duplicated$PIDN_cleaned <- as.vector(fixed_pidn)
+  duplicated$SampleID_cleaned <- as.vector(fixed_pidn)
+  duplicated
+}
+
+metadata_proteomics_fixed <- rowwise(metadata_proteomics_plus) %>% group_map(fix_id) %>% bind_rows
+metadata_proteomics_recombined <- bind_rows(metadata_proteomics_no_plus, metadata_proteomics_fixed)
+
+metadata_proteomics_recombined$PIDN_files <- metadata_proteomics_recombined$PIDN_cleaned
+metadata_proteomics_recombined$PIDN_files[!is_in(metadata_proteomics_recombined$PIDN_files, genotypes$ID)] <- NA
+
+metadata_proteomics_recombined$SampleID_files <- metadata_proteomics_recombined$SampleID_cleaned
+metadata_proteomics_recombined$SampleID_files[!is_in(metadata_proteomics_recombined$SampleID_files, genotypes$ID)] <- NA
+
+metadata_proteomics_recombined$ADRC_ID_files <- metadata_proteomics_recombined$ADRC_ID_cleaned
+metadata_proteomics_recombined$ADRC_ID_files[!is_in(metadata_proteomics_recombined$ADRC_ID_files, genotypes$ID)] <- NA
+
+get_unique_value <- function(row, row_df) {
+  unique_ids <- c(row$PIDN_files, row$SampleID_files, row$ADRC_ID_files) %>% na.omit %>% unique
+  row$filename <- str_c(unique_ids, collapse = ";")
+  row
+}
+
+metadata_proteomics_filenames <- rowwise(metadata_proteomics_recombined) %>% group_map(get_unique_value) %>% bind_rows
+metadata_proteomics_filenames$filename[nchar(metadata_proteomics_filenames$filename) == 0] <- NA
+filter(metadata_proteomics_filenames, str_detect(unique_id, ";"))
+
+tissues <- filter(metadata_proteomics_filenames, !is.na(Esophagus)) 
+tissues_tally <- group_by(tissues, Individual_ID) %>% tally
+#nine have 2- which ones do we use?
+
+#metadata_proteomics_filenames this is the version that has all of the information
+#we are now going to filter out the rows that do not have tissue data
+
+#what columns do we care about- how do we combine things such as dx that change over multiple visits?
+group_and_concat <- annotated_metadata %>%
+  dplyr::select(match, SCG_ID, SampleId,	Individual_ID,	Age,	ADRC_ID,PIDN,	Visit,	Gender,	Diagnosis_group, Diagnosis_consensus,Date.of.draw,	Study) %>% 
+  group_by(Individual_ID) %>%
+  mutate(SampleIds = paste(SampleId, collapse = ", ")) %>%
+  mutate(Ages = paste(Age, collapse = ", ")) %>%
+  mutate(ADRC_IDs = paste(ADRC_ID, collapse = ", ")) %>% 
+  mutate(PIDNs = paste(PIDN, collapse = ", ")) %>% 
+  mutate(Visits = paste(Visit, collapse = ", ")) %>% 
+  mutate(Diagnoses_consensus = paste(Diagnosis_consensus, collapse = ", ")) %>% 
+  mutate(Diagnosis_groups = paste(Diagnosis_group, collapse = ", ")) %>% 
+  mutate(Date.of.draws = paste(Date.of.draw, collapse = " | ")) %>% 
+  mutate(Studies = paste(Study, collapse = ", ")) %>% 
+  mutate(Ages = paste(SCG_ID, collapse = ", ")) %>%
+  mutate(matches = paste(match, collapse = "")) 
+
+
+group_and_concats <- group_and_concat %>% dplyr::select(matches, Individual_ID, SampleIds, ADRC_IDs, PIDNs, Ages, Gender, Visits, Diagnosis_group, Diagnoses_consensus, Date.of.draws, Studies)
+
+
 ##############################################################################################################
 ####################################-------FIND DIFF BTW THE LISTS-------#####################################
 ##############################################################################################################
-individual_id <- setdiff(genotypes, metadata_id_df)
+individual_id <- setdiff(genotypes$ID, metadata_proteomics_filenames$filename)
 #"415" "424" "465" "474" "563" "565" "583" "584" "610" -> these all belong to the "IndividualId?
 missing_genotypes <- c(415, 424, 465, 474, 563, 565, 583, 584, 610)
 
@@ -158,62 +141,28 @@ genotypes_filtered_list_unnammed <- unname(genotypes_filtered_list) %>% unlist()
 setdiff(genotypes_filtered, metadata_id_df)
 #none, which is what we expect
 
+
 ##############################################################################################################
 ####################################-------ADD COLUMN TO METADATA------#######################################
 ####################################-------SAYS IF IS IN GENOTYPES------######################################
 ####################################-------DOES NOT INCLUDE INDIV ID------#####################################
 ##############################################################################################################
-metadata_ids$match <- as.numeric(metadata_ids$ID %in% genotypes_filtered_list_unnammed)
-
-PIDN_matched <- metadata_ids %>% filter(match == 1 & grepl("PIDN", index_val))
-PIDN_matched <- PIDN_matched %>% 
-  mutate(PIDN_ID = ifelse(!grepl("3900", ID), paste0("0", ID), ID))
-PIDN_no_matched <- metadata_ids %>% filter(match == 0 & grepl("PIDN", index_val))
-PIDN_no_matched$PIDN_ID <- NA
-PIDN_combined <- rbind(PIDN_matched, PIDN_no_matched) %>% select(index, PIDN_ID)
+write_tsv(data.frame(annotated_metadata), "/Users/maurertm/Desktop/Projects/ADRC/annotated_metadata_April_7.tsv")
 
 
-ADRC_matched <- metadata_ids %>% filter(match == 1 & grepl("ADRC", index_val))
-ADRC_matched <- ADRC_matched %>% 
-  mutate(ADRC_ID = ifelse(!grepl("3900", ID), paste0("0", ID), ID))
-ADRC_no_matched <- metadata_ids %>% filter(match == 0 & grepl("ADRC", index_val))
-ADRC_no_matched$ADRC_ID <- NA
-ADRC_combined <- rbind(ADRC_matched, ADRC_no_matched) %>% select(index, ADRC_ID)
 
 
-SI_matched <- metadata_ids %>% filter(match == 1 & grepl("SI", index_val))
-SI_matched <- SI_matched %>% 
-  mutate(SI_ID = ifelse(!grepl("3900", ID), paste0("0", ID), ID))
-SI_no_matched <- metadata_ids %>% filter(match == 0 & grepl("SI", index_val))
-SI_no_matched$SI_ID <- NA
-SI_combined <- rbind(SI_matched, SI_no_matched) %>% select(index, SI_ID)
 
-PIDN_ADRC <- merge(PIDN_combined, ADRC_combined, by="index")
+CHIP_calls_file <- "/Users/maurertm/Downloads/mutect_aggregated_noWL_Apr_5.tsv"
+CHIP_calls <-  read_tsv(CHIP_calls_file)
 
-complete_ID <- merge(PIDN_ADRC, SI_combined, by="index")
 
-#complete_ID[is.na(complete_ID)] <- 0
+CHIP_calls$filename <- str_remove_all(CHIP_calls$Sample, "^0") %>% str_remove_all("\\.hg38") %>%
+  str_remove_all(" .*$") %>% str_remove_all("_.*$") %>% str_remove_all("[A-Z]") 
 
-#grouped_metadata_index <- aggregate(match ~ index, metadata_index, sum)
+CHIP_calls_filtered <- filter(CHIP_calls, t_ref_count > 0 & 
+                              tumor_f <= 0.35 & longest_repeat < 5 &
+                              is_in(vcf_FILTER, c("PASS", "weak_evidence", "clustered_events", "clustered_events;weak_evidence")))
 
-cols <- c('PIDN_ID' , 'ADRC_ID' , 'SI_ID')
-complete_ID$SCG_IDs<- apply( complete_ID[ , cols ] , 1 , paste , collapse = " " )
-complete_merged<- complete_ID %>% 
-  mutate(new_id = ifelse(grepl("[1-9]", SCG_IDs), gsub("NA","",as.character(complete_ID$SCG_IDs)), NA)) %>% 
-  mutate(sequenced = ifelse(grepl("[1-9]", new_id), "Yes", "No")) %>% 
-  mutate(SCG_ID = new_id) %>%
-  select(index, SCG_ID, sequenced) %>%
-  merge(grouped_metadata_index, by = "index")
+CHIP_proteomics <- left_join(metadata_proteomics_filenames, CHIP_calls)
 
-complete_edited <- complete_merged %>%
-  mutate(SCG_IDs = ifelse(grepl("'^T'", SCG_ID), substring(SCG_ID, 2), SCG_ID)) %>%
-  separate(SCG_IDs, into = c("SCG_ID1", "SCG_ID2"), sep = " (?=[^ ]+$)") %>%
-  select(index, SCG_ID1, sequenced, match)
-
-#all SCG1 = SCG2
-
-proteomics_distinct_rows$index <- rownames(proteomics_distinct_rows)
-
-annotated_metadata <- merge(complete_edited, proteomics_distinct_rows, by = "index")
-
-write_tsv(data.frame(annotated_metadata), "/Users/maurertm/Desktop/Projects/ADRC/annotated_metadata_April_1.tsv")
